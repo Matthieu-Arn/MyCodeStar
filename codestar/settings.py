0b2375ffff92c19b5a22ad662d9f8478e443bfbf
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-09-16 09:09:16 +0000

    Create procfile and prepare for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_#1ix-hqx9_5vpii^bivi5zg%slrp!6wjsjo-2o4qqjc^jhs9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-matthieuarn-mycodestar-fo4ak2bbcce.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-matthieuarn-mycodestar-fo4ak2bbcce.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
 
 # Application definition
